31bb2568bcf37327fb3bd67fa1c979f8bfab6b00
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-04-22 16:42:59 +0800

    更正项目启动类的类名: SkyTakeApplication

--- sky-take-server/src/main/java/org/qiaice/SkyTakeApplication.java
+++ sky-take-server/src/main/java/org/qiaice/SkyTakeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SkeTakeApplication {
+public class SkyTakeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SkeTakeApplication.class, args);
+        SpringApplication.run(SkyTakeApplication.class, args);
     }
 }
